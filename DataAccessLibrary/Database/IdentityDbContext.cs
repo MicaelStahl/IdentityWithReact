67311acc35655ce2da6691d3d38aae22c9044798
Micael Ståhl <Micael_Stahl96@hotmail.com>
2019-10-18 12:12:58 +0200

    Add project files.

---
+++ DataAccessLibrary/Database/IdentityDbContext.cs
@@ -0,0 +1,21 @@
+﻿using DataAccessLibrary.Models;
+using IdentityServer4.EntityFramework.Options;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccessLibrary.Database
+{
+    internal class IdentityDbContext : ApiAuthorizationDbContext<AppUser>
+    {
+        public IdentityDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions)
+            : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
---
+++ DataAccessLibrary/Models/AppUser.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace DataAccessLibrary.Models
+{
+    /// <summary>
+    /// Default ApplicationUser for this application.
+    /// </summary>
+    public class AppUser : IdentityUser
+    {
+        [Required]
+        [EmailAddress]
+        [DataType(DataType.EmailAddress)]
+        public override string UserName { get => base.UserName; set => base.UserName = Email; }
+
+        [Required]
+        [Display(Name = "Firstname")]
+        [StringLength(20, ErrorMessage = "The Firstname can only hold a max length of {1} and min length of {2} characters.", MinimumLength = 2)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [Display(Name = "Lastname")]
+        [StringLength(30, ErrorMessage = "The lastname can only hold a max length of {1} and min length of {2} characters.", MinimumLength = 2)]
+        public string LastName { get; set; }
+    }
+}
